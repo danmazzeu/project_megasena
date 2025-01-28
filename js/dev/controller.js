55d7b63e6845b932df65d11dcbbe72e728925259
Daniel Mazzeu <danmazzeu9@gmail.com>
2025-01-28 15:56:13 -0300

    Uplaod

--- js/dev/controller.js
+++ js/dev/controller.js
@@ -129,12 +129,14 @@ analysisContestForm.addEventListener('submit', (event) => {
     });
 });
 
-// Consult Contest
+// Consult Sequence
 const consultSequenceForm = document.getElementById('consult-sequence');
 const consultSequenceInput = document.getElementById('consult-sequence-input');
+const consultSequenceError = document.getElementById('consult-sequence-error');
 
 consultSequenceInput.addEventListener('input', () => {
     consultSequenceInput.classList.remove('errorField');
+    consultSequenceError.style.display = 'none';
 });
 
 consultSequenceForm.addEventListener('submit', (event) => {
@@ -143,6 +145,25 @@ consultSequenceForm.addEventListener('submit', (event) => {
         consultSequenceInput.classList.add('errorField');
     } else {
         consultSequenceInput.classList.remove('errorField');
+
+        const inputSequence = consultSequenceInput.value.split(', ').map(Number); 
+        const sequenceExists = parsedData.some(item => {
+            const itemDezenas = item.listaDezenas.map(dezena => Number(dezena)); 
+            return itemDezenas.every(dezena => inputSequence.includes(dezena)) && 
+                   inputSequence.every(dezena => itemDezenas.includes(dezena));
+        });
+
+        if (sequenceExists) {
+            consultSequenceError.classList.add('fail');
+            consultSequenceError.classList.remove('success');
+            consultSequenceError.querySelector('p').textContent = "Sequência já sorteada anteriormente."
+        } else {
+            consultSequenceError.classList.add("success");
+            consultSequenceError.classList.remove('fail');
+            consultSequenceError.querySelector('p').textContent = "Sequência não sorteada anteriormente."
+        }
+
+        consultSequenceError.style.display = 'flex';
     }
 });
 
