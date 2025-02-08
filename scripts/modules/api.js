b9307c2be32375d9bac031b1c2715675aea4eebf
Daniel Mazzeu <danmazzeu9@gmail.com>
2025-02-07 21:20:49 -0300

    upload

--- scripts/modules/api.js
+++ scripts/modules/api.js
@@ -3,7 +3,7 @@ import { loading } from "./loading.js";
 async function api() {
     loading(true, 'Sincronizando dados', 'Aguarde enquanto analisamos todos os dados dos concursos, para gerar todas as estatísticas e dados desta página.');
     try {
-        const response = await fetch('https://loteriascaixa-api.herokuapp.com/api/megasena/');
+        const response = await fetch('projectmegasena-production.up.railway.app');
         const data = await response.json();
         const allData = data.map(contest => ({ ...contest }));
         loading(false);
