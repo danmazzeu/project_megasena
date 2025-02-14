e6b724f4235bd0b82af990bf3a544092a743af95
Daniel Mazzeu <danmazzeu9@gmail.com>
2025-02-13 23:53:13 -0300

    Upload

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import axios from 'axios';
+import fs from 'fs/promises';
 
 const app = express();
 const port = 3001;
@@ -12,13 +13,17 @@ app.get('/', async (req, res) => {
         const response = await axios.get('https://lotasda-api.herokuapp.com/api/megasena');
         res.json(response.data);
     } catch (error) {
-        const backupData = '';
         console.error("Proxy Server Error:", error);
-        fs.readFile('backup.json', 'utf8', (err, data) => {
-            backupData = JSON.parse(data);
-            console.log('bateu aqui');
-        });
-        res.json(backupData);
+        
+        try {
+            const data = await fs.readFile('backup.json', 'utf8');
+            const backupData = JSON.parse(data);
+            console.log('Backup data loaded successfully');
+            res.json(backupData);
+        } catch (readError) {
+            console.error("Error reading or parsing backup file:", readError);
+            res.status(500).json({ message: "Unable to fetch data from both the API and the backup." });
+        }
     }
 });
 
