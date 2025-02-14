0e17e08df3e3512c456e2dedfe6b43f1761cc41e
Daniel Mazzeu <danmazzeu9@gmail.com>
2025-02-13 23:41:36 -0300

    Upload

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.get('/', async (req, res) => {
             console.error("Mega Sena API Error:", error.response.status, error.response.data);
         }
         
-        fs.readFile(path.join('backup.json'), 'utf8', (err, data) => {
+        fs.readFile('backup.json'), 'utf8', (err, data) => {
             if (err) {
                 console.error("Error reading backup file:", err);
                 return res.status(500).json({ error: 'Error fetching data from API and backup' });
