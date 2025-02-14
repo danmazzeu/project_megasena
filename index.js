214049c216266a8f4aee3b3e32edf9502109ef1e
Daniel Mazzeu <danmazzeu9@gmail.com>
2025-02-13 23:34:36 -0300

    Upload

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const fs = require('fs');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 
