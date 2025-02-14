cfb67f9967b641562650baa7729c2bedfe51b0f4
Daniel Mazzeu <danmazzeu9@gmail.com>
2025-02-13 22:10:28 -0300

    Upload

--- scripts/modules/api.js
+++ scripts/modules/api.js
@@ -1,4 +1,5 @@
 import { loading } from "./loading.js";
+import * as fs from 'fs/promises';
 
 async function api() {
     loading(true, 'Sincronizando dados', 'Aguarde...');
@@ -9,12 +10,29 @@ async function api() {
             throw new Error(`API request failed with status ${response.status}: ${errorText}`);
         }
         const data = await response.json();
+
+        try {
+            await fs.writeFile('backup.json', JSON.stringify(data, null, 2), { flag: 'w' });
+            console.log("Backup atualizado com sucesso.");
+        } catch (error) {
+            console.error("Erro ao salvar o backup:", error);
+        }
+
         loading(false);
         return data;
     } catch (error) {
-        console.error("Error fetching data:", error);
-        loading(true, 'Manutenção', 'Servidor temporariamente em manutenção. Tente novamente mais tarde.');
-        return [];
+        console.error("Erro ao buscar dados:", error);
+
+        try {
+            const backupData = await fs.readFile('backup.json', 'utf8');
+            console.log("Dados carregados do backup.");
+            loading(false);
+            return JSON.parse(backupData);
+        } catch (backupError) {
+            console.error("Erro ao ler o arquivo de backup:", backupError);
+            loading(true, 'Manutenção', 'Servidor temporariamente em manutenção. Tente novamente mais tarde.');
+            return [];
+        }
     }
 }
 
