67b4b2dfd773e5cd951c929367a6b6801c64a8a1
Daniel Mazzeu <danmazzeu9@gmail.com>
2025-02-13 23:15:15 -0300

    Upload

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 import express from 'express';
 import cors from 'cors';
 import axios from 'axios';
-import fs from 'fs';
+import { promises as fs } from 'fs'; // Importando fs.promises
 import path from 'path';
 
 const app = express();
 const backupFile = path.join(__dirname, 'backup.json');
-const port = 3001;
+const port = process.env.PORT || 3000;
 
 const allowedOrigins = [
     'https://danmazzeu.github.io',
     'https://megalumni.com.br',
     'https://www.megalumni.com.br',
-    'http://localhost'
+    'http://localhost:3000'
 ];
 
 app.use(cors({
@@ -28,15 +28,24 @@ app.use(cors({
 app.get('/', async (req, res) => {
     try {
         const response = await axios.get('https://loteriascaixa-api.herokuapp.com/api/megasena');
-        fs.writeFileSync(backupFile, JSON.stringify(response.data), 'utf8');
+        
+        // Escrever o backup de forma assíncrona
+        await fs.writeFile(backupFile, JSON.stringify(response.data), 'utf8');
+        
+        // Envia os dados da API
         res.json(response.data);
     } catch (error) {
         console.error("Erro ao buscar dados da API:", error);
-    
-        if (fs.existsSync(backupFile)) {
-            const backupData = fs.readFileSync(backupFile, 'utf8');
+
+        try {
+            // Verifica se existe o arquivo de backup
+            await fs.access(backupFile);
+
+            // Lê os dados do backup de forma assíncrona
+            const backupData = await fs.readFile(backupFile, 'utf8');
             res.json(JSON.parse(backupData));
-        } else {
+        } catch (readError) {
+            // Se não houver backup e falhar a API, retorna erro
             res.status(500).json({ error: 'Erro ao buscar dados da API e não há backup disponível' });
         }
     }
